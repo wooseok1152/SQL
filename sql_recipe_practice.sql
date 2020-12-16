4273a2db22c813e676f86ba9b1ff7994db26c5a7
wooseok1152 <banggoo1152@naver.com>
2020-12-16 16:57:55 +0900

    129 page finished

--- sql_recipe_practice.sql
+++ sql_recipe_practice.sql
@@ -127,7 +127,7 @@ SELECT * FROM ACCESS_LOG;
 
 WITH VIEWDATA AS(SELECT CASE WHEN SUBSTR(URL, INSTR(URL, '/', 1, 3), INSTR(URL, '?', 1, 1) - INSTR(URL, '/', 1, 3)) IS NULL THEN ' ' ELSE SUBSTR(URL, INSTR(URL, '/', 1, 3), INSTR(URL, '?', 1, 1) - INSTR(URL, '/', 1, 3))END AS RESULT1, CASE WHEN SUBSTR(URL, INSTR(URL, '/', 1, 3), INSTR(URL, '#', 1, 1) - INSTR(URL, '/', 1, 3)) IS NULL THEN ' ' ELSE SUBSTR(URL, INSTR(URL, '/', 1, 3), INSTR(URL, '#', 1, 1) - INSTR(URL, '/', 1, 3)) END AS RESULT2 FROM ACCESS_LOG) SELECT RESULT1||RESULT2 AS FINAL FROM VIEWDATA;
 
-/* WINDOW함수 사용법 */
+/* 기본적인 WINDOW함수 사용법 */
 DROP TABLE IF EXISTS review;
 CREATE TABLE review (
     user_id    varchar(255)
@@ -147,6 +147,205 @@ INSERT INTO review VALUES('U003', 'A003', 4.0);
 
 SELECT * FROM REVIEW;
 SELECT USER_ID, SCORE, ROUND(AVG(SCORE) OVER()) AS AVG_WITH_WHOLE, ROUND(AVG(SCORE) OVER(PARTITION BY USER_ID)) AS AVG_BY_USER_ID FROM REVIEW;
+
+/* PARTITION BY구문과 ORDER BY구문을 동시에 사용하는 WINDOW함수 활용법 */
+DROP TABLE IF EXISTS popular_products;
+CREATE TABLE popular_products (
+    product_id varchar(255)
+  , category   varchar(255)
+  , score      numeric
+);
+
+INSERT INTO popular_products VALUES('A001', 'action', 94);
+INSERT INTO popular_products VALUES('A002', 'action', 81);
+INSERT INTO popular_products VALUES('A003', 'action', 78);
+INSERT INTO popular_products VALUES('A004', 'action', 64);
+INSERT INTO popular_products VALUES('D001', 'drama' , 90);
+INSERT INTO popular_products VALUES('D002', 'drama' , 82);
+INSERT INTO popular_products VALUES('D003', 'drama' , 78);
+INSERT INTO popular_products VALUES('D004', 'drama' , 58);
+COMMIT;
+
+WITH WINDOW_RANKED_TABLE AS(SELECT CATEGORY, PRODUCT_ID, SCORE, ROW_NUMBER() OVER (PARTITION BY CATEGORY ORDER BY SCORE DESC) AS RANK FROM POPULAR_PRODUCTS) 
+SELECT CATEGORY, PRODUCT_ID FROM WINDOW_RANKED_TABLE WHERE RANK = 1 ORDER BY CATEGORY DESC;
+
+/* 레코드를 열로 변환하기 */
+DROP TABLE IF EXISTS daily_kpi;
+CREATE TABLE daily_kpi (
+    dt        varchar(255)
+  , indicator varchar(255)
+  , val       NUMBER(10)
+);
+
+INSERT INTO daily_kpi VALUES('2017-01-01', 'impressions', 1800);
+INSERT INTO daily_kpi VALUES('2017-01-01', 'sessions'   ,  500);
+INSERT INTO daily_kpi VALUES('2017-01-01', 'users'      ,  200);
+INSERT INTO daily_kpi VALUES('2017-01-02', 'impressions', 2000);
+INSERT INTO daily_kpi VALUES('2017-01-02', 'sessions'   ,  700);
+INSERT INTO daily_kpi VALUES('2017-01-02', 'users'      ,  250);
+COMMIT;
+
+SELECT * FROM daily_kpi;
+SELECT DT, MAX(CASE WHEN INDICATOR = 'IMPRESSIONS' THEN VAL END) AS IMPRESSIONS, MAX(CASE WHEN INDICATOR = 'SESSIONS' THEN VAL END) AS sessions, MAX(CASE WHEN INDICATOR = 'USERS' THEN VAL END) AS users FROM daily_kpi GROUP BY DT ORDER BY DT;
+
+/* 특정 레코드들을 쉼표로 구분한 문자열로 집약하기(LISTAGG()함수 사용법) */
+DROP TABLE IF EXISTS purchase_detail_log;
+CREATE TABLE purchase_detail_log 
+(
+    purchase_id integer
+  , product_id varchar(255)
+  , price integer
+);
+
+INSERT INTO purchase_detail_log VALUES(100001, 'A001', 300);
+INSERT INTO purchase_detail_log VALUES(100001, 'A002', 400);
+INSERT INTO purchase_detail_log VALUES(100001, 'A003', 200);
+INSERT INTO purchase_detail_log VALUES(100002, 'D001', 500);
+INSERT INTO purchase_detail_log VALUES(100002, 'D001', 300);
+INSERT INTO purchase_detail_log VALUES(100003, 'A001', 300);
+
+SELECT * FROM PURCHASE_DETAIL_LOG;
+SELECT PURCHASE_ID, LISTAGG(PRODUCT_ID, ', ') WITHIN GROUP(ORDER BY PRODUCT_ID) FROM PURCHASE_DETAIL_LOG GROUP BY PURCHASE_ID; 
+
+/* 열로 표현된 값을 행으로 변환하기 */
+DROP TABLE IF EXISTS quarterly_sales2;
+CREATE TABLE quarterly_sales2 (
+    year integer
+  , q1   integer
+  , q2   integer
+  , q3   integer
+  , q4   integer
+);
+
+INSERT INTO quarterly_sales2 VALUES (2015, 82000, 83000, 78000, 83000);
+INSERT INTO quarterly_sales2 VALUES (2016, 85000, 85000, 80000, 81000);
+INSERT INTO quarterly_sales2 VALUES (2017, 92000, 81000, NULL , NULL );
+
+SELECT Q.YEAR, CASE WHEN P.IDX = 1 THEN 'Q1' WHEN P.IDX = 2 THEN 'Q2' WHEN P.IDX = 3 THEN 'Q3' WHEN P.IDX = 4 THEN 'Q4' END AS QUARTER, CASE WHEN P.IDX = 1 THEN Q.Q1 WHEN P.IDX = 2 THEN Q.Q2 WHEN P.IDX = 3 THEN Q.Q3 WHEN P.IDX = 4 THEN Q.Q4 END AS SALES FROM QUARTERLY_SALES2 Q CROSS JOIN (SELECT 1 AS IDX FROM DUAL UNION ALL SELECT 2 AS IDX FROM DUAL UNION ALL SELECT 3 AS IDX FROM DUAL UNION ALL SELECT 4 AS IDX FROM DUAL) P;
+
+/* JOIN을 사용하여 두 테이블을 가로로 연결하기 */
+DROP TABLE IF EXISTS mst_categories;
+CREATE TABLE mst_categories (
+    category_id integer
+  , name        varchar(255)
+);
+
+INSERT INTO mst_categories VALUES(1, 'dvd');
+INSERT INTO mst_categories VALUES(2, 'cd');
+INSERT INTO mst_categories VALUES(3, 'book');
+
+DROP TABLE IF EXISTS category_sales;
+CREATE TABLE category_sales (
+    category_id integer
+  , sales       integer
+);
+
+INSERT INTO category_sales VALUES(1, 850000);
+INSERT INTO category_sales VALUES(2, 500000);
+
+DROP TABLE IF EXISTS product_sale_ranking;
+CREATE TABLE product_sale_ranking (
+    category_id integer
+  , rank        integer
+  , product_id  varchar(255)
+  , sales       integer
+);
+
+INSERT INTO product_sale_ranking VALUES(1, 1, 'D001', 50000);
+INSERT INTO product_sale_ranking VALUES(1, 2, 'D002', 20000);
+INSERT INTO product_sale_ranking VALUES(1, 3, 'D003', 10000);
+INSERT INTO product_sale_ranking VALUES(2, 1, 'C001', 30000);
+INSERT INTO product_sale_ranking VALUES(2, 2, 'C002', 20000);
+INSERT INTO product_sale_ranking VALUES(2, 3, 'C003', 10000);
+
+SELECT * FROM MST_CATEGORIES M JOIN CATEGORY_SALES S ON m.category_id = s.category_id JOIN PRODUCT_SALE_RANKING R ON M.CATEGORY_ID = R.CATEGORY_ID;
+SELECT M.CATEGORY_ID, M.NAME, R.PRODUCT_ID, S.SALES FROM (MST_CATEGORIES M LEFT JOIN PRODUCT_SALE_RANKING R ON m.category_id = R.CATEGORY_ID) LEFT JOIN CATEGORY_SALES S ON M.CATEGORY_ID = S.CATEGORY_ID WHERE R.RANK = 1 OR R.RANK IS NULL;
+
+/* 조건 플래그를 0과 1로 표현하기 */
+DROP TABLE IF EXISTS mst_users_with_card_number;
+CREATE TABLE mst_users_with_card_number 
+(
+    user_id     varchar(255)
+  , card_number varchar(255)
+);
+
+INSERT INTO mst_users_with_card_number VALUES('U001', '1234-xxxx-xxxx-xxxx');
+INSERT INTO mst_users_with_card_number VALUES('U002', NULL                 );
+INSERT INTO mst_users_with_card_number VALUES('U003', '5678-xxxx-xxxx-xxxx');
+
+DROP TABLE IF EXISTS purchase_log;
+CREATE TABLE purchase_log 
+(
+    purchase_id integer
+  , user_id     varchar(255)
+  , amount      integer
+  , stamp       varchar(255)
+);
+
+INSERT INTO purchase_log VALUES(10001, 'U001', 200, '2017-01-30 10:00:00');
+INSERT INTO purchase_log VALUES(10002, 'U001', 500, '2017-02-10 10:00:00');
+INSERT INTO purchase_log VALUES(10003, 'U001', 200, '2017-02-12 10:00:00');
+INSERT INTO purchase_log VALUES(10004, 'U002', 800, '2017-03-01 10:00:00');
+INSERT INTO purchase_log VALUES(10005, 'U002', 400, '2017-03-02 10:00:00');
+
+SELECT * FROM MST_USERS_WITH_CARD_NUMBER M LEFT JOIN PURCHASE_LOG P ON M.USER_ID = P.user_id;
+SELECT M.USER_ID, M.CARD_NUMBER, COUNT(P.PURCHASE_ID) AS PURCHASE_COUNT, CASE WHEN COUNT(P.PURCHASE_ID) = 0 THEN 0 ELSE 1 END AS HAS_PURCHASED,  CASE WHEN COUNT(M.CARD_NUMBER) = 0 THEN 0 ELSE 1 END AS HAS_CARD FROM MST_USERS_WITH_CARD_NUMBER M LEFT JOIN PURCHASE_LOG P ON M.USER_ID = P.user_id GROUP BY M.USER_ID, M.CARD_NUMBER;
+
+/* WITH구문 활용하는 법 */
+DROP TABLE IF EXISTS product_sales;
+CREATE TABLE product_sales 
+(
+    category_name varchar(255)
+  , product_id    varchar(255)
+  , sales         integer
+);
+
+INSERT INTO product_sales VALUES('dvd' , 'D001', 50000);
+INSERT INTO product_sales VALUES('dvd' , 'D002', 20000);
+INSERT INTO product_sales VALUES('dvd' , 'D003', 10000);
+INSERT INTO product_sales VALUES('cd'  , 'C001', 30000);
+INSERT INTO product_sales VALUES('cd'  , 'C002', 20000);
+INSERT INTO product_sales VALUES('cd'  , 'C003', 10000);
+INSERT INTO product_sales VALUES('book', 'B001', 20000);
+INSERT INTO product_sales VALUES('book', 'B002', 15000);
+INSERT INTO product_sales VALUES('book', 'B003', 10000);
+INSERT INTO product_sales VALUES('book', 'B004',  5000);
+COMMIT;
+
+SELECT * FROM PRODUCT_SALES;
+
+-- 카테고리별 순위를 추가한 테이블 조회
+SELECT CATEGORY_NAME, SALES, ROW_NUMBER() 
+OVER(PARTITION BY CATEGORY_NAME ORDER BY SALES) 
+AS RANK 
+FROM PRODUCT_SALES;
+
+-- 카테고리들의 순위에서 유니크한 순위 목록을 조회
+WITH RANKED_PRODUCT_SALES AS
+(
+SELECT CATEGORY_NAME, SALES, ROW_NUMBER() 
+OVER(PARTITION BY CATEGORY_NAME ORDER BY SALES) 
+AS RANK 
+FROM PRODUCT_SALES
+), MST_RANK AS
+(SELECT DISTINCT RANK 
+FROM RANKED_PRODUCT_SALES
+) 
+SELECT * FROM MST_RANK;
+
+-- 
+WITH RANKED_PRODUCT_SALES AS
+(
+SELECT CATEGORY_NAME, SALES, ROW_NUMBER() 
+OVER(PARTITION BY CATEGORY_NAME ORDER BY SALES) 
+AS RANK 
+FROM PRODUCT_SALES
+), MST_RANK AS
+(SELECT DISTINCT RANK 
+FROM RANKED_PRODUCT_SALES
+) 
+SELECT * 
+FROM MST_RANK AS M LEFT JOIN RANKED_PRODUCT_SALES R1 ON M.RANK = R1.RANK AND R1.CATEGORY_NAME = 'DVD';
 
 
 
