38c2d11b47e84afd131aa5f30393cb2537dc8e45
wooseok1152 <banggoo1152@naver.com>
2021-03-29 23:44:09 +0900

    materialize hint update

--- oracle_hint_practice.sql
+++ oracle_hint_practice.sql
@@ -10,6 +10,9 @@ FROM PROFESSOR;
 SELECT *
 FROM COURSE;
 
+SELECT *
+FROM SCGRADE;
+
 /*
 LEADING힌트를 사용해서, 테이블 접근 순서를 설정할 수 있음
 먼저 접근된 테이블이 Join시 Driving 테이블이 됨
@@ -108,6 +111,150 @@ SELECT /*+ INLINE NO_MERGE) */
        ORDERS
 FROM SECOND_WITH A, SCORE B
 WHERE A.CNO = B.CNO
+)
+
+SELECT *
+FROM THIRD_WITH;
+
+/* 
+참조되지 않은 View에 대해선 아무런 Operation이 발생하지 않음
+*/  
+WITH FIRST_WITH AS(
+SELECT /*+ INLINE NO_MERGE FULL(COURSE) PARALLEL(COURSE 4)*/
+       CNO,
+       CNAME,
+       PNO,
+       ROW_NUMBER() OVER(ORDER BY CNO DESC) AS RNK
+FROM COURSE
+WHERE CNO LIKE '%2%'
+),
+
+SECOND_WITH AS (
+SELECT /*+ INLINE NO_MERGE */
+       SNO,
+       CNO,
+       RESULT,
+       (SELECT /*+ NO_UNNEST */
+               GRADE 
+        FROM SCGRADE B
+        WHERE B.HISCORE >= A.RESULT
+              AND B.LOSCORE <= A.RESULT
+       ) AS GRADE
+FROM SCORE A
+)
+
+SELECT *
+FROM SECOND_WITH;
+
+SELECT * FROM TABLE(DBMS_XPLAN.DISPLAY_CURSOR(NULL, NULL, 'ALLSTATS LAST'));
+
+/* 
+참조된 inline view갯수만큼, 'VIEW'operation이 발생함
+두 번째 inline view에선 단지 첫 번째 inline view 결과에 조건을 기입하였지만, 해당 inline view에 대해 'VIEW'operation이 발생함
+*/  
+WITH FIRST_WITH AS(
+SELECT /*+ INLINE NO_MERGE FULL(COURSE) PARALLEL(COURSE 4)*/
+       CNO,
+       CNAME,
+       PNO,
+       ROW_NUMBER() OVER(ORDER BY CNO DESC) AS RNK
+FROM COURSE
+),
+
+SECOND_WITH AS (
+SELECT /*+ INLINE NO_MERGE */
+       *
+FROM FIRST_WITH
+WHERE TO_CHAR(RNK) LIKE '%1%'
+)
+
+SELECT *
+FROM SECOND_WITH;
+
+SELECT * FROM TABLE(DBMS_XPLAN.DISPLAY_CURSOR(NULL, NULL, 'ALLSTATS LAST'));
+
+/* 
+두 개의 view에 대해 'HASH JOIN'operation이 발생될 수 있음
+Correlated Subquery에 대해선, 특정 join operation이 발생되지 않음
+*/  
+WITH FIRST_WITH AS(
+SELECT /*+ INLINE NO_MERGE FULL(COURSE) PARALLEL(COURSE 4)*/
+       CNO,
+       CNAME,
+       PNO,
+       ROW_NUMBER() OVER(ORDER BY CNO DESC) AS RNK
+FROM COURSE
+WHERE CNO LIKE '%2%'
+),
+
+SECOND_WITH AS (
+SELECT /*+ INLINE NO_MERGE */
+       SNO,
+       CNO,
+       RESULT,
+       (SELECT /*+  NO_UNNEST */
+               GRADE 
+        FROM SCGRADE B
+        WHERE B.HISCORE >= A.RESULT
+              AND B.LOSCORE <= A.RESULT
+       ) AS GRADE
+FROM SCORE A
+),
+
+THIRDE_WITH AS (
+SELECT /*+ INLINE NO_MERGE */
+       B.SNO,
+       A.CNAME,
+       A.PNO,
+       B.RESULT,
+       B.GRADE
+FROM FIRST_WITH A, SECOND_WITH B
+WHERE A.CNO = B.CNO
+      AND TO_CHAR(RNK) LIKE '%1%'
+)
+
+SELECT B.SNAME,
+       A.CNAME,
+       A.RESULT,
+       A.GRADE
+FROM THIRDE_WITH A, STUDENT B
+WHERE A.SNO = B.SNO;
+
+SELECT * FROM TABLE(DBMS_XPLAN.DISPLAY_CURSOR(NULL, NULL, 'ALLSTATS LAST'));
+
+
+/* 
+'MATERIALZE'힌트로 인해 생성된 임시 테이블에 대해선, Table Scan이 발생함
+*/  
+WITH FIRST_WITH AS(
+SELECT /*+ MATERIALIZE */
+       CNO,
+       CNAME,
+       PNO,
+       ROW_NUMBER() OVER(ORDER BY CNO DESC) AS RNK
+FROM COURSE
+WHERE CNO LIKE '%2%'
+),
+
+SECOND_WITH AS (
+SELECT /*+ MATERIALIZE */
+       CNO,
+       CNAME,
+       PNO,
+       RNK
+FROM FIRST_WITH
+WHERE TO_CHAR(RNK) LIKE '%1%'
+),
+
+THIRD_WITH AS (
+SELECT /*+ MATERIALIZE */
+       A.CNO,
+       A.CNAME,
+       B.PNAME,
+       B.ORDERS,
+       A.RNK
+FROM SECOND_WITH A, PROFESSOR B
+WHERE A.PNO = B.PNO
 )
 
 SELECT *
