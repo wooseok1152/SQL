32d1c1acc83c2ec45d1727510fc8d7f75a84a5c0
wooseok1152 <banggoo1152@naver.com>
2022-07-17 22:07:20 +0900

    add a logic of getting CVR by page

--- SQL_Recipe_sample-code/script/select/6장/사이트 전체의 특징(경향) 찾기.sql
+++ SQL_Recipe_sample-code/script/select/6장/사이트 전체의 특징(경향) 찾기.sql
@@ -1,4 +1,4 @@
--- 날짜별 접근 데이터를 집계하는 쿼리
+-- 주어진 기간 內, 날짜별 접근 데이터를 집계하는 쿼리
 
 SELECT SUBSTR(STAMP, 1, 10) AS BASE_DATE,
        COUNT(DISTINCT LONG_SESSION) AS ACCESS_USER_CNT,
@@ -8,7 +8,7 @@ FROM ACCESS_LOG
 GROUP BY SUBSTR(STAMP, 1, 10)
 ORDER BY BASE_DATE;
 
--- URL별로 집계하는 쿼리
+-- 주어진 기간 內, URL별로 집계하는 쿼리
 
 SELECT URL,
        COUNT(DISTINCT LONG_SESSION) AS ACCESS_USER_CNT,
@@ -18,7 +18,7 @@ FROM ACCESS_LOG
 GROUP BY URL
 ORDER BY URL;
 
--- 경로별로 집계하는 쿼리
+-- 주어진 기간 內, 경로별로 집계하는 쿼리
 
 SELECT URL_PATH,
        COUNT(DISTINCT LONG_SESSION) AS ACCESS_USER_CNT,
@@ -34,7 +34,7 @@ FROM (
 GROUP BY URL_PATH
 ORDER BY URL_PATH;
 
--- URL에 의미를 부여해서 집계하는 쿼리
+-- 주어진 기간 內, URL에 의미를 부여해서 집계하는 쿼리
 
 SELECT PAGE_NAME,
        COUNT(DISTINCT LONG_SESSION) AS ACCESS_USER_CNT,
@@ -76,3 +76,42 @@ FROM (
 GROUP BY PAGE_NAME
 ORDER BY PAGE_NAME
 ;
+
+-- 유입원별로 방문 횟수를 집계하는 쿼리
+
+SELECT VIA,
+       COUNT(1) AS ACCESS_CNT
+FROM (
+    SELECT  STAMP,
+            LONG_SESSION,
+            SHORT_SESSION,
+            CASE 
+                WHEN URL_UTM_SOURCE IS NOT NULL AND URL_UTM_MEDIUM IS NOT NULL
+                     THEN URL_UTM_SOURCE || '-' || URL_UTM_MEDIUM
+                ELSE CASE 
+                          WHEN REGEXP_SUBSTR(REFERRER_DOMAIN, 'google') IS NOT NULL OR REGEXP_SUBSTR(REFERRER_DOMAIN, 'yahoo') IS NOT NULL 
+                               THEN 'search'
+                          WHEN REGEXP_SUBSTR(REFERRER_DOMAIN, 'twitter') IS NOT NULL OR REGEXP_SUBSTR(REFERRER_DOMAIN, 'facebook') IS NOT NULL 
+                               THEN 'social'
+                          ELSE 'other'
+                     END
+                END AS VIA
+    FROM (
+        SELECT URL,
+               REGEXP_REPLACE(REGEXP_SUBSTR(URL, 'https?://[^/]*'), 'https?://', '') AS URL_DOMAIN,
+               REGEXP_REPLACE(REGEXP_SUBSTR(URL, 'utm_source=[^&]*'), 'utm_source=', '') AS URL_UTM_SOURCE,
+               REGEXP_REPLACE(REGEXP_SUBSTR(URL, 'utm_medium=[^&]*'), 'utm_medium=', '') AS URL_UTM_MEDIUM,
+               REGEXP_REPLACE(REGEXP_SUBSTR(REFERRER, 'https?://[^/]*'), 'https?://', '') AS REFERRER_DOMAIN,
+               LONG_SESSION,
+               SHORT_SESSION,
+               STAMP
+        FROM   ACCESS_LOG
+    )
+    WHERE REFERRER_DOMAIN IS NOT NULL
+          AND REFERRER_DOMAIN != URL_DOMAIN
+)
+GROUP BY VIA
+ORDER BY ACCESS_CNT DESC
+;
+
+-- 
